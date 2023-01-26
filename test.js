2c888f8749faa408fea95a7c5a7f0664d27fbb59
takashi0825 <118641983+takashi0825@users.noreply.github.com>
2023-01-26 19:51:11 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 alert(location.href);
+function save_file() {
+    Array.prototype.forEach.call(
+        document.getElementsByClassName('TextWithTruncation__TheButton-sc-1wolf3g-0'), function(elem){elem.click()}
+    );
+    Array.prototype.forEach.call(
+        document.getElementsByClassName('ProfileItems__Container-sc-1gso5r4-0'), function(elem) {elem.click()}
+    );
+    str_url = location.href;
+    var sspage=(new XMLSerializer).serializeToString(document),
+    blob=new Blob([sspage], {type:"text/plain"}),
+    url=window.URL,
+    bloburl=url.createObjectURL(blob),
+    a=document.createElement("a");
+    a.download=str_url+'.html';
+    a.href=bloburl;
+    a.click();
+}
+save_file();
